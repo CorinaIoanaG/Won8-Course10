7c3e99269dda975173c6a8cb15d580dcbc3fa8f3
CorinaIoanaG <corinaghete@gnail.com>
2022-10-03 18:02:20 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,25 @@ public class Main {
         ClassRoom classRoom = new ClassRoom();
         StudentGrade studentGrade;
         ReportGenerator reportGenerator = new ReportGenerator();
+
         studentGrade = classRoom.getStudentList().get(0);
         System.out.println(String.format("First line: %s, %s, %d", studentGrade.getName(), studentGrade.getDiscipline(),
                 studentGrade.getGrade()));
+
         System.out.println(String.format("Grades for student %s %s", "Branwen Evelina",
                 classRoom.getGradesForStudent("Branwen Evelina").toString()));
+
         studentGrade = classRoom.getMaxGrade(null);
         System.out.println(String.format("Maximum grade: %s %s %d", studentGrade.getName(),
                 studentGrade.getDiscipline(), studentGrade.getGrade()));
+
         System.out.println(String.format("Grades from discipline %s %s", "Mathematics",
                 classRoom.getGradesForDiscipline("Mathematics").toString()));
+
         System.out.println(String.format("Grades for discipline %s: maximum %d, average %d, mimimum %d", "Mathematics",
                 classRoom.getMaxGrade("Mathematics").getGrade(), classRoom.getAverageGrade("Mathematics"),
                 classRoom.getWorstGrade("Mathematics").getGrade()));
+
         reportGenerator.generateReport();
     }
 }
--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -3,18 +3,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class ReportGenerator {
+    ClassRoom classRoom = new ClassRoom();
+
     public ReportGenerator() throws IOException {
     }
 
     public void generateReport() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("files/grade-reports.out"));
-        ClassRoom classRoom = new ClassRoom();
         StudentGrade studentGrade;
         studentGrade = classRoom.getMaxGrade(null);
         writer.write(String.format("Cea mai mare nota: %s %d", studentGrade.getName(), studentGrade.getGrade()));
         writer.newLine();
+
         writer.write(String.format("Nota medie: %d", classRoom.getAverageGrade(null)));
         writer.newLine();
+
         studentGrade = classRoom.getWorstGrade(null);
         writer.write(String.format("Cea mai mica nota: %s %d", studentGrade.getName(), studentGrade.getGrade()));
         writer.close();
